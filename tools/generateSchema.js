3d8b3a69da1e93776b175a54364d6032a5075646
Pablo Segura <pablo.segura@aol.com>
2020-02-17 15:23:43 +0400

    Initial Commit

--- tools/generateSchema.js
+++
@@ -1,6 +0,0 @@
-const { makeExecutableSchema } = require('graphql-tools');
-
-const typeDefs = require('../src/graphql/typeDefs');
-const resolvers = require('../src/graphql/resolvers');
-
-module.exports = makeExecutableSchema({ typeDefs, resolvers });
